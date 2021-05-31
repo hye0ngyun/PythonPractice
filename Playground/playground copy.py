15bb1ddc96e1709dd19255e3ccb4a7f598e28f2b
shg7271 <shg7271@gmail.com>
2021-05-31 17:38:13 +0900

    learn python

---
+++ Playground/playground copy.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup as bs4
+import requests
+
+# #old_content > table > tbody
+url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
+data = requests.get(url)
+s = bs4(data, 'html.parser')
+print(s)
+
+# print(data.text)
