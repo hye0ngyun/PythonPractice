009a1f7b4e65e8403a8a6561c4fd99dacdd344b4
Shin <shg7271@gmail.com>
2022-05-30 17:50:50 +0900

    Update: publishing & input apply comma work & radio check condition

--- projects/Money_Saver/test.py
+++ projects/Money_Saver/test.py
@@ -6,11 +6,27 @@ def calculate(*args):
   # 부양가족 수
   dependents = Element('dependents').value
   # 비과세액
-  taxfree = Element('taxfree').value
+  taxfree = int(Element('taxfree').value.replace(',', ''))
   # 세전월급
   el_before_tax_salary = Element('salary')
   before_tax_salary = el_before_tax_salary.value
   before_tax_salary = int(before_tax_salary.replace(',', ''))
+
+  # 급여기준
+  print(Element('sal-year').element.checked)
+  sal_year = Element('sal-year').element.checked
+  severance_pay = Element('severance-pay-false').element.checked
+  # 연봉, 별도
+  if sal_year and severance_pay:
+    before_tax_salary //= 12
+  # 연봉, 포함
+  elif sal_year and not severance_pay:
+    before_tax_salary //= 13
+
+    
+
+
+
 
   # 세전월급과 부양가족수로 각 공제금 구하기
   deductions = get_deduction(before_tax_salary, dependents, taxfree)
@@ -29,4 +45,5 @@ def calculate(*args):
   Element('고용보험').element.innerText = deductions['고용보험']
   Element('소득세').element.innerText = deductions['소득세']
   Element('지방소득세').element.innerText = deductions['지방소득세']
+
   
